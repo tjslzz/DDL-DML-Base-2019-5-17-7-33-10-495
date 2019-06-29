e3743f96d745a8f60a836b167aa1a3fa4aab4d1d
JarryLi <3244188372@qq.com>
2019-06-29 15:16:00 +0800

    update sql_file

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,15 @@
--- **数据库级别：**  
+﻿-- **数据库级别：**  
 --  显示所有数据库  
-
+SHOW DATABASES;
 --  进入某个数据库  
-
+USE student_examination_sys;
 --  创建一个数据库  
-
+CREATE DATABASE student_examination_sys;
 --  创建指定字符集的数据库  
-
+CREATE DATABASE student_examination_sys DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 --  显示数据库的创建信息   
-
+SHOW CREATE DATABASE student_examination_sys;
 --  修改数据库的编码  
-
+character-set-server=utf8
 --  删除一个数据库   
-
--- **表级别**
---  修改表名
-
---  修改字段的数据类型
-
---  修改字段名
-
---  添加字段
-
---  删除字段
-
---  修改表的存储引擎
-
---  删除表的外键约束
-
---  删除一张表
+DROP DATABASE student_examination_sys;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
--- 插入记录  
-
+﻿-- 插入记录  
+INSERT INTO `student` VALUES ('001', '张三', 18, '男');
 -- 修改记录  
-
+UPDATE `student` SET name='jarryli' WHERE id = '001'；
 --  删除记录  
-
--- 查询记录  
+DELETE FROM `student` WHERE id = '001';
+-- 查询记录 
+SELECT * FROM `student`  ;
